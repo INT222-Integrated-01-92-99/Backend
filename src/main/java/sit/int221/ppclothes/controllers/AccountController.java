cf4c04ebe09568596d4fd23ba245e899a5525263
F <60165717+Ffriend@users.noreply.github.com>
2021-09-25 00:47:10 +0700

    Update AccountController.java

--- src/main/java/sit/int221/ppclothes/controllers/AccountController.java
+++ src/main/java/sit/int221/ppclothes/controllers/AccountController.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8081")
 public class AccountController {
     @Autowired
     private repoAccount repoAccount;
