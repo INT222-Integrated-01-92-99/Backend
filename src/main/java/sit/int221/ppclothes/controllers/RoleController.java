a0b8a56bf80b13a7f2c50df75457d2e832d6814a
nanonachino-522 <60164853+nanonachino-522@users.noreply.github.com>
2021-11-09 14:20:38 +0700

    edit RoleController

--- src/main/java/sit/int221/ppclothes/controllers/RoleController.java
+++ src/main/java/sit/int221/ppclothes/controllers/RoleController.java
@@ -1,2 +1,19 @@
-package sit.int221.ppclothes.controllers;public class RoleController {
+package sit.int221.ppclothes.controllers;
+
+import org.springframework.web.bind.annotation.*;
+import sit.int221.ppclothes.models.Role;
+import sit.int221.ppclothes.repositories.repoRole;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class RoleController {
+    @Autowired
+    private repoRole repoRole;
+
+    @GetMapping ("/Role")
+    public List<Role> Role(){
+        return repoRole.findAll();
+    }
+
 }
