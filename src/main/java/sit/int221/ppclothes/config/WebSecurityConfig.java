280846817d0a4fd4d3160b45f74253ac1f458b31
nanonachino-522 <60164853+nanonachino-522@users.noreply.github.com>
2021-11-20 18:46:50 +0700

    fix Merge

--- src/main/java/sit/int221/ppclothes/config/WebSecurityConfig.java
+++ src/main/java/sit/int221/ppclothes/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ package sit.int221.ppclothes.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -16,6 +17,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +44,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
+    @Value("#{'${clothes.origin.method}'.split(',')}")
+    private String[] methodList;
+    @Value("#{'${clothes.origin.host}'.split(',')}")
+    String[] hostList;
+    @Value("#{'${clothes.origin.headers}'.split(',')}")
+    String[] headersList;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -55,14 +66,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // We don't need CSRF for this example
-        httpSecurity.csrf().disable()
+        httpSecurity.cors(
+                        config -> {
+                            CorsConfiguration cors = new CorsConfiguration();
+                            cors.setAllowCredentials(true);
+                            cors.setAllowedOrigins(Arrays.asList(hostList));
+                            cors.setAllowedMethods(Arrays.asList(methodList));
+                            cors.setAllowedHeaders(Arrays.asList(headersList));
+
+                            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                            source.registerCorsConfiguration("/**", cors);
+
+                            config.configurationSource(source);
+                        }
+                ).csrf().disable()
                 // dont authenticate this particular request
                 .authorizeRequests()
                 .antMatchers("/main/**").permitAll()
                 .antMatchers("/member/**").hasRole("MEMBER")
                 .antMatchers("/staff/**").hasRole("STAFF")
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/adsta/**").hasAnyRole("ADMIN","STAFF").
+                .antMatchers("/adsta/**").hasAnyRole("ADMIN","STAFF")
+                .antMatchers("/admem/**").hasAnyRole("ADMIN","MEMBER")
+                .antMatchers("/allroles/**").hasAnyRole("ADMIN","STAFF","MEMBER").
                 // all other requests need to be authenticated
                         and().
                 // make sure we use stateless session; session won't be used to
