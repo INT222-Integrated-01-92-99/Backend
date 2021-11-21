942c923cfa8015d6c0b8d471cb390ccc4ef2aa0e
nanonachino-522 <60164853+nanonachino-522@users.noreply.github.com>
2021-11-21 11:21:02 +0700

    fix for frontend

--- src/main/java/sit/int221/ppclothes/controllers/AccountController.java
+++ src/main/java/sit/int221/ppclothes/controllers/AccountController.java
@@ -2,21 +2,15 @@ package sit.int221.ppclothes.controllers;
 
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-import sit.int221.ppclothes.exceptions.AccountException;
 import sit.int221.ppclothes.exceptions.ExceptionRepo;
 import sit.int221.ppclothes.exceptions.ProductException;
 import sit.int221.ppclothes.models.*;
 import sit.int221.ppclothes.repositories.repoAccount;
-import sit.int221.ppclothes.repositories.repoRole;
 import sit.int221.ppclothes.repositories.repoCart;
-import sit.int221.ppclothes.repositories.repoCartDetails;
-import sit.int221.ppclothes.repositories.repoReceipt;
-import sit.int221.ppclothes.repositories.repoReceiptDetails;
+import sit.int221.ppclothes.repositories.repoRole;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.lang.reflect.Array;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,14 +21,7 @@ public class AccountController {
     @Autowired
     private repoCart repoCart;
     @Autowired
-    private repoCartDetails repoCartDetails;
-    @Autowired
-    private repoReceipt repoReceipt;
-    @Autowired
-    private repoReceiptDetails repoReceiptDetails;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private repoRole repoRole;
 
     @GetMapping("/admin/account")
     public List<Account> Account(){
@@ -50,13 +37,9 @@ public class AccountController {
 
     @PostMapping(value = "/main/registaccount")
     public Account Register(@RequestBody Account newAccount){
-        if(repoAccount.findByAccUsername(newAccount.getAccUsername()) != null){
-            throw new AccountException(ExceptionRepo.ERROR_CODE.USERNAME_HAVE_ALREADY,"Username have already!!");
-        }
         long newIDcart = repoCart.getMaxCartId() + 1;
         long newIDaccount = repoAccount.getMaxidAcc() + 1;
-        String encodedpassword = passwordEncoder.encode(newAccount.getAccPass());
-        Account newAccountnoReceipt = new Account(newIDaccount,newAccount.getAccUsername(),encodedpassword,newAccount.getAccFname(), newAccount.getAccLname(), newAccount.getAccPhone(), newAccount.getAccAddress(),newAccount.getIdRole());
+        Account newAccountnoReceipt = new Account(newIDaccount,newAccount.getAccUsername(),newAccount.getAccPass(),newAccount.getAccFname(), newAccount.getAccLname(), newAccount.getAccPhone(), newAccount.getAccAddress(),newAccount.getIdRole());
         Cart newCart = new Cart(newIDcart,0);
         repoCart.save(newCart);
         newAccountnoReceipt.setCart(newCart);
@@ -66,34 +49,12 @@ public class AccountController {
 
     @PutMapping(value = "/allroles/editaccount")
     public Account EditAccount(@RequestBody Account editAccount){
-        String EditEncodedPassword = passwordEncoder.encode(editAccount.getAccPass());
-        editAccount.setAccPass(EditEncodedPassword);
         return repoAccount.save(editAccount);
     }
 
 
     @DeleteMapping(value = "/admin/deleteaccount")
     public void DelAccount(@RequestParam long idAccount){
-        Account account = repoAccount.findById(idAccount).orElse(null);
-        Cart cart = account.getCart();
-        if( cart.getCartDetails() != null ){
-            List<CartDetails> cartDetailsList = cart.getCartDetails();
-            for (CartDetails cartDetailsperline : cartDetailsList){
-                repoCartDetails.deleteById(cartDetailsperline.getIdCartDetail());
-            }
-        }
-        repoCart.delete(cart);
-
-        if( account.getReceiptList() != null ){
-            List<Receipt> receiptList = account.getReceiptList();
-            for(Receipt receiptperline : receiptList){
-                List<ReceiptDetails> receiptDetailsList = receiptperline.getReceiptDetailsList();
-                for(ReceiptDetails receiptDetailsperline : receiptDetailsList){
-                    repoReceiptDetails.deleteById(receiptDetailsperline.getIdReceiptDetails());
-                }
-                repoReceipt.deleteById(receiptperline.getIdReceipt());
-            }
-        }
         repoAccount.deleteById(idAccount);
     }
 
